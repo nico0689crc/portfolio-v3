2a308b827b9d95743528304a1784d257f8131a28
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-04-22 10:33:41 -0300

    Ajustes de imagenes

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,16 @@ const nextConfig: NextConfig = {
         locale: false
       }
     ]
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
