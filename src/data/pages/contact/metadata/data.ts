7058e19cac1e9169133102988de51503b0f34f98
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-05-08 09:54:38 -0300

    Actualizando ciudad

--- src/data/pages/contact/metadata/data.ts
+++ src/data/pages/contact/metadata/data.ts
@@ -9,11 +9,11 @@ export const metadata: MetadataType = {
   en: {
     title: 'Conctact',
     description:
-      "Get in touch with Nicolas Ariel Fernandez, a Fullstack Developer from Neuquen, Argentina. Whether you're interested in collaborating on a project, learning more about my work, or discussing job opportunities, feel free to reach out via email, LinkedIn, or GitHub. Let's connect!",
+      "Get in touch with Nicolas Ariel Fernandez, a Fullstack Developer from Argentina. Whether you're interested in collaborating on a project, learning more about my work, or discussing job opportunities, feel free to reach out via email, LinkedIn, or GitHub. Let's connect!",
   },
   es: {
     title: 'Contacto',
     description:
-      'Ponte en contacto con Nicolas Ariel Fernandez, un Desarrollador Fullstack de Neuquén, Argentina. Si estás interesado en colaborar en un proyecto, aprender más sobre mi trabajo o discutir oportunidades laborales, no dudes en comunicarte a través de correo electrónico, LinkedIn o GitHub. ¡Conectemos!',
+      'Ponte en contacto con Nicolas Ariel Fernandez, un Desarrollador Fullstack de  Argentina. Si estás interesado en colaborar en un proyecto, aprender más sobre mi trabajo o discutir oportunidades laborales, no dudes en comunicarte a través de correo electrónico, LinkedIn o GitHub. ¡Conectemos!',
   },
 };
